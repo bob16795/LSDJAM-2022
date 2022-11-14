055be0bc8c0a1f5331020a3194d7e25fcbfd8ff1
bob16795 <prestosilver.ptp@gmail.com>
2022-11-14 10:03:41 -0500

    fix build to be release instead of debug lol

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf out/content.bin
 cp -r content.bin content/debug.json out
 
-nim c --app:gui --opt:speed -d:debug -d:ssl --threads:on -d:mingw -o:out/main.exe main
-nim c --app:gui --opt:speed -d:debug -d:ssl --threads:on main
+nim c --app:gui --opt:speed -d:release -d:ssl --threads:on -d:mingw -o:out/main.exe main
+nim c --app:gui --opt:speed -d:release -d:ssl --threads:on main
